524b6c914be974912c77effd3926bc83367f628a
Christine Koppelt <ch.ko123@gmail.com>
2012-04-13 00:35:29 +0200

    added basic data model

--- techism/events/models.py
+++ techism/events/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.name;
+    
+    
+class Organization (models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    description = models.TextField(blank=True, null=True)
+        
+    def __unicode__(self):
+        return self.title;
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    date_time_begin = models.DateTimeField()
+    date_time_end = models.DateTimeField(blank=True, null=True)
+    url = models.URLField()
+    description = models.TextField(blank=True, null=True)
+    location = models.ForeignKey(Location, blank=True, null=True)
+    archived = models.BooleanField()
+    published = models.BooleanField()
+    canceled = models.BooleanField()
+    date_time_created = models.DateTimeField(auto_now_add=True)
+    date_time_modified = models.DateTimeField(auto_now=True)
+    organization = models.ForeignKey(Organization, blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.title
+          
--- techism/techism/settings.py
+++ techism/techism/settings.py
@@ -29,18 +29,10 @@ DATABASES = {
 # system time zone.
 TIME_ZONE = 'America/Chicago'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
 
+LANGUAGE_CODE = 'de-DE'
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
@@ -118,6 +110,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
