205a6960223b7dcf5d0baad275c2b7fe84d8967b
Bernd Plontsch <bernd.plontsch@gmail.com>
2012-04-18 21:19:17 +0200

    static dummy html folder created

--- techism/techism/urls.py
+++ techism/techism/urls.py
@@ -8,10 +8,11 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'techism.views.home', name='home'),
     # url(r'^techism/', include('techism.foo.urls')),
-
+	
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    (r'^dummy', 'django.views.generic.simple.direct_to_template', {'template': 'dummy/index.html'}),
 )
