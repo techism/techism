224609d626ee5e51cf3936bbf9417bf3ca82aa24
seelmann <mail@stefan-seelmann.de>
2012-04-18 23:13:52 +0200

    Remove template, use static instead

--- techism/techism/settings.py
+++ techism/techism/settings.py
@@ -62,6 +62,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    'static/',
 )
 
 # List of finder classes that know how to find static files in
@@ -101,7 +102,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'techism/techism/templates/',
 )
 
 INSTALLED_APPS = (
