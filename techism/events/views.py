8ca5e19e60b7c57fb52bc7a8ac427d5eaadb16bb
Christine Koppelt <ch.ko123@gmail.com>
2012-09-10 01:38:58 +0200

    added first view

--- techism/events/views.py
+++ techism/events/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def index(request):
+    return render_to_response('events/index.html', {}, context_instance=RequestContext(request))
--- techism/techism/settings.py
+++ techism/techism/settings.py
@@ -99,9 +99,7 @@ ROOT_URLCONF = 'techism.urls'
 WSGI_APPLICATION = 'techism.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
