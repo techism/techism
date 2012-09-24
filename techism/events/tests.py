a4731b714667146567ea2835d0933d221ed1e623
Christine Koppelt <ch.ko123@gmail.com>
2012-09-25 00:45:56 +0200

    added initial version for ical feed

--- techism/events/tests.py
+++ techism/events/tests.py
@@ -4,7 +4,7 @@
 """
 
 from django.test import TestCase
-from event_service import get_current_tags
+from event_service import get_current_tags, get_upcomming_published_events_query_set
 
 
 class SimpleTest(TestCase):
@@ -14,13 +14,17 @@ class SimpleTest(TestCase):
     def test_tags(self):
         it = get_current_tags()
         tag1 = it.next()
-        self.assertEqual("java", tag1.name)
-        self.assertEqual(1, tag1.num_tags)
+        self.assertEqual(tag1.name, "java")
+        self.assertEqual(tag1.num_tags, 1)
         tag2 = it.next()
-        self.assertEqual("python", tag2.name)
-        self.assertEqual(1, tag2.num_tags)
+        self.assertEqual(tag2.name, "python")
+        self.assertEqual(tag2.num_tags, 1)
         tag3 = it.next()
-        self.assertEqual("test", tag3.name)
-        self.assertEqual(2, tag3.num_tags)
+        self.assertEqual(tag3.name, "test")
+        self.assertEqual(tag3.num_tags, 2)
         with self.assertRaises(StopIteration): 
             it.next()
+            
+    def test_upcomming_events(self):
+        events = get_upcomming_published_events_query_set()
+        self.assertEqual(events.count(), 2)
--- techism/urls.py
+++ techism/urls.py
@@ -11,10 +11,10 @@ urlpatterns = patterns('',
     # static pages
     (r'^impressum/$', direct_to_template, { 'template': 'impressum.html' }),
     (r'^about/$', direct_to_template, { 'template': 'about.html' }),
+    
+    # iCal
+    (r'^feed.ics$', 'techism.feeds.views.ical'),
 
-    # Examples:
-    # url(r'^$', 'techism.views.home', name='home'),
-    # url(r'^techism/', include('techism.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
