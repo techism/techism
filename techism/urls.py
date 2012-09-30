7921bca987427d952b201340077fc9f1195a82ae
Christine Koppelt <ch.ko123@gmail.com>
2012-09-30 14:34:29 +0200

    added rss and atom feeds to base.html and url

--- techism/urls.py
+++ techism/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
+from techism.rss.feeds import UpcommingEventsRssFeed, UpcommingEventsAtomFeed
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,9 +14,14 @@ urlpatterns = patterns('',
     (r'^about/$', direct_to_template, { 'template': 'about.html' }),
     
     # iCal
-    (r'^feed.ics$', 'techism.feeds.views.ical'),
-    (r'^ical/(?P<event_id>.+).ics$', 'techism.feeds.views.ical_single_event'),
+    (r'^feed.ics$', 'techism.ical.views.ical'),
+    (r'^ical/(?P<event_id>.+).ics$', 'techism.ical.views.ical_single_event'),
 
+    # Atom
+    (r'^feeds/atom/upcomming_events$', UpcommingEventsAtomFeed()),
+    
+    #RSS
+    (r'^feeds/rss/upcomming_events$', UpcommingEventsRssFeed()),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
