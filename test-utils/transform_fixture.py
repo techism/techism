bdcd771f972b4b7678c7d41716c02a35517be51a
Stefan Seelmann <mail@stefan-seelmann.de>
2012-11-04 00:45:03 +0100

    Implemented replacement of date, formatted fixture template

--- test-utils/transform_fixture.py
+++ test-utils/transform_fixture.py
@@ -2,25 +2,32 @@
 # -*- coding: utf-8 -*-
 import re
 import datetime
-import time
 
+def replace_date(pattern, days_offset, text):
+    p = re.compile(pattern)
+    date = datetime.date.today() + datetime.timedelta(days=days_offset)
+    str = date.strftime("%Y-%m-%d")
+    result = p.sub(str, text)
+    return result
+    
 #Open file
 in_file = open ('fixture_template.json', 'r')
 text = in_file.read()
 
 #Replace 'YESTERDAY'
-p = re.compile ('TOMORROW')
-tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-tomorrow_str = time.strftime("%Y-%m-%dT19:00:00.000Z")
-result = p.sub(tomorrow_str, text)
-
+text = replace_date('YESTERDAY', -1, text)
 
 #Replace 'TOMORROW'
-
+text = replace_date('TOMORROW', 1, text)
 
 #Replace 'NEXTWEEK'
+text = replace_date('NEXTWEEK', 7, text)
 
 
 out_file = open('fixture.json', 'w')
-out_file.write(result)
+out_file.write(text)
 out_file.close()
+
+
+
+    
