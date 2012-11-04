6076add8524e4bb3d6aa1e9685c1a4fd0a760f1d
Stefan Seelmann <mail@stefan-seelmann.de>
2012-11-04 19:19:49 +0100

    Generate test fixture before test run

--- manage.py
+++ manage.py
@@ -3,6 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
+    # create test fixture first
+    if len(sys.argv) > 1 and sys.argv[1] == "test":
+        os.chdir("test-utils")
+        execfile("transform_fixture.py")
+        os.chdir("..")
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techism.settings")
 
     from django.core.management import execute_from_command_line
