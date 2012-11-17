f60fe58233717da9913e8495e502f063e28329e1
Christine Koppelt <ch.ko123@googlemail.com>
2012-11-17 13:34:35 +0100

    added static openstreetmap to create page

--- static/js/events/create.js
+++ static/js/events/create.js
@@ -1,3 +1,10 @@
+var geocoder;
+var map;
+var locations;
+
+	initializeMunichCityCenter();
+
+
 $(function() {
 	$.datepicker.setDefaults( $.datepicker.regional[ "de" ] );
 	$("#id_date_time_begin_0").datepicker( $.datepicker.regional[ "de" ]);
--- static/js/maps.js
+++ static/js/maps.js
@@ -1,3 +1,43 @@
+
+
+function displayLocation(street, city){
+  var where = $("#map_location");
+  var location = street +","+city+","+ "Bayern";
+  if (where[0].nodeName == "DIV"){
+    if (street.length > 0 && city.length > 0){
+        deleteOverlays();
+        geocodeAndSetMarker (map, location, true);
+    }
+  } else {
+    var width = where.parent().width(); // img of #map_location may not yet be loaded, use parent
+    var height = getStaticMapHeight(width);
+    var newMap = '<img id="map_location" src="http://maps.google.com/maps/api/staticmap?center=' + location + '&size='+width+'x'+height+'&zoom=15&sensor=false&markers=color:red|' + location + '" />';
+    where.replaceWith(newMap);
+  }
+}
+
+
+function initializeMunichCityCenter() {
+  var where = $("#map_canvas");
+  var width = where.width();
+  var mapId = "map_location";
+  //if(width < 640) {
+    // static map with link for small screens, max. size of static map is 640x640
+    var height = getStaticMapHeight(width);
+    var lat = "48.13788";
+    var lon = "11.575953";
+    where.append('<img src="http://staticmap.openstreetmap.de/staticmap.php?center='+lat+','+lon+'&zoom=15&size='+width+'x'+height+'" />');
+  //}
+  //else {
+    // dynamic map for larger screens
+  //  var height = getDynamicMapHeight(width);
+  //  where.append('<div id="'+mapId+'" style="height: '+height+'px; width: 100%" />');
+  //  var myOptions = getOptionsMunichCityCenter ();
+  //  map = new google.maps.Map(document.getElementById(mapId), myOptions);
+  //}
+}
+
+
 function renderEventDetailMap() {
 	var where = $(this).children("section.where");
 	
