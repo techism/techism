f70513293f47fd5d661acd41ca390e81f8eba466
Stefan Seelmann <mail@stefan-seelmann.de>
2012-12-02 02:21:26 +0100

    Fix chicken-egg problem: DB table can't be read before syncdb

--- techism/settings/database.py
+++ techism/settings/database.py
@@ -9,4 +9,5 @@ try:
     TWITTER_CONSUMER_KEY = settings_service.get_twitter_consumer_key_for_login()
     TWITTER_CONSUMER_SECRET = settings_service.get_twitter_consumer_secret_for_login()
 except DatabaseError:
-    pass
+    from django.db import connection
+    connection._rollback()
