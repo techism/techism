e8542f993aace1a8fedc84b5247f95edfa0923a9
Christine Koppelt <ch.ko123@googlemail.com>
2012-12-04 19:19:50 +0100

    added Content Security Policy

--- techism/settings/base.py
+++ techism/settings/base.py
@@ -56,6 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'techism.middleware.SecureRequiredMiddleware',
+    'techism.middleware.ContentSecurityPolicyMiddlerware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
