13f7e177c45e60ab4e4a39a2ba1d54827d776c61
Stefan Seelmann <mail@stefan-seelmann.de>
2012-12-24 21:27:36 +0100

    Improvements in admin

--- techism/admin.py
+++ techism/admin.py
@@ -4,14 +4,30 @@ from techism.models import Event, EventTag, Location, Organization, Organization
 from django.contrib import admin
 import reversion
 
+
+class EventInline(admin.TabularInline):
+    model = Event
+    fields = ['title', 'date_time_begin', 'date_time_end', 'url', 'tags', 'published']
+    readonly_fields = fields
+    ordering = ['-date_time_begin']
+    can_delete = False
+    max_num = 0
+    
 class LocationAdmin(admin.ModelAdmin):
     search_fields = ['name']
-    list_display = ['name', 'street', 'city', 'latitude', 'longitude']
+    list_display = ['name', 'street', 'city', 'latitude', 'longitude', 'historized_since']
+    ordering = ['historized_since', 'name']
+    inlines = [
+        EventInline,
+    ]
 
 class EventAdmin(reversion.VersionAdmin):
     search_fields = ['title']
     list_filter = ['published']
     list_display = ['title', 'date_time_begin', 'date_time_end', 'location', 'user', 'published']
+    date_hierarchy = 'date_time_begin'
+    filter_horizontal = ['tags']
+    raw_id_fields = ['location', 'user', 'organization']
 
 class EventTagAdmin(admin.ModelAdmin):
     search_fields = ['name']
