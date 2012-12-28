ea2a1fbf2b63a88e1b7c59c523e1d6649f60dce6
Stefan Seelmann <mail@stefan-seelmann.de>
2012-12-29 00:11:35 +0100

    Append next page

--- static/js/organizations/index.js
+++ static/js/organizations/index.js
@@ -1,5 +1,47 @@
 $(function(){
+  // masonry: fluent boxes
   $('#contentorganizations').masonry({
     itemSelector : '.organizations'
   });
+
+  // infinite scrolling
+  $('#contentorganizations').infinitescroll({
+    navSelector  : ".next",  // selector for the paged navigation (it will be hidden)
+    nextSelector : ".next",  // selector for the NEXT link (to page 2)
+    itemSelector : ".organizations", // selector for all items you'll retrieve
+    errorCallback: function(){ $('#infscr-loading').remove() }
+    },
+    function(newElements){
+      // hide new items while they are loading
+      var $newElems = $( newElements ).css({ opacity: 0 });
+      // ensure that images load before adding to masonry layout
+      $newElems.imagesLoaded(function(){
+        // show elems now they're ready
+        $newElems.animate({ opacity: 1 });
+        $('#contentorganizations').masonry('reload');
+      });
+      // add ajax click handler to more link
+      var pagina = newElements[newElements.length - 1];
+      // add click handler or remove pagina when there is no next page
+      var next = $('.next', pagina);
+      if (next.length > 0){
+        next.bind('click', moreHandler);
+      }
+  });
+
+  function moreHandler(e){
+    e.preventDefault();
+
+    // remove old pagina
+    $(this).parent().parent().parent().remove();
+
+    $(document).trigger('retrieve.infscr');
+  }
+
+  // kill scroll binding
+  $(window).unbind('.infscr');
+
+  // hook up the manual click guy.
+  $('.next').bind('click', moreHandler);
+
 });
