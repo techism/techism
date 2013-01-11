c91871b9cfdf4e40600b1fbcd8bd70af896da24d
Stefan Seelmann <mail@stefan-seelmann.de>
2013-01-11 02:27:44 +0100

    Remove unused settings

--- techism/settings/database.py
+++ techism/settings/database.py
@@ -8,8 +8,6 @@ try:
     SECRET_KEY = settings_service.get_secret_key()
     TWITTER_CONSUMER_KEY = settings_service.get_twitter_consumer_key_for_login()
     TWITTER_CONSUMER_SECRET = settings_service.get_twitter_consumer_secret_for_login()
-    TWITTER_ACCESS_KEY = settings_service.get_twitter_access_key_for_tweets()
-    TWITTER_ACCESS_SECRET = settings_service.get_twitter_access_secret_for_tweets()
 except DatabaseError:
     from django.db import connection
     connection._rollback()
