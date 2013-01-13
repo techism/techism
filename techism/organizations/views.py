9d5ab8eab1a73b86ba4c383e9258f05c9ab8ac1f
Stefan Seelmann <mail@stefan-seelmann.de>
2013-01-13 22:02:02 +0100

    Fix IDE warnings

--- techism/organizations/views.py
+++ techism/organizations/views.py
@@ -3,13 +3,13 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from techism.organizations import organization_service
-from techism.models import Organization, OrganizationTag
+from techism.models import OrganizationTag
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 def index(request):
-    list = organization_service.get_all()
+    org_list = organization_service.get_all()
     tags = organization_service.get_tags()
-    page = __get_paginator_page(request, list)
+    page = __get_paginator_page(request, org_list)
     if page == -1:
         return HttpResponseNotFound()
     return render_to_response(
@@ -23,12 +23,12 @@ def index(request):
 
 def tag(request, tag_name):
     tag = get_object_or_404(OrganizationTag, name=tag_name)
-    list = organization_service.get_by_tag(tag)
+    org_list = organization_service.get_by_tag(tag)
     tags = organization_service.get_tags()
     return render_to_response(
         'organizations/index.html', 
         {
-            'organization_list': list, 
+            'organization_list': org_list, 
             'tags': tags, 
             'tag_name': tag_name
         }, 
