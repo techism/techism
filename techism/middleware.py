2b79d1642b2495a2284d4791b73f856e6591ccd8
Stefan Seelmann <mail@stefan-seelmann.de>
2013-01-13 19:49:23 +0100

    Needed for chrome

--- techism/middleware.py
+++ techism/middleware.py
@@ -41,6 +41,7 @@ class ContentSecurityPolicyMiddleware(object):
         
         standard_policy = "default-src 'self';" \
             "img-src 'self' *.tile.openstreetmap.org staticmap.openstreetmap.de;" \
+            "style-src 'self' 'unsafe-inline';" \
             "connect-src 'self' nominatim.openstreetmap.org;" \
             "xhr-src 'self' nominatim.openstreetmap.org;" 
         #header for firefox and Internet Explorer 
