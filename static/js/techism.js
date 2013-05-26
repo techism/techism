89de41f5150f4644d833946fc303f87773920054
Stefan Seelmann <mail@stefan-seelmann.de>
2013-05-26 18:13:14 +0200

    Add Piwik tracking

--- static/js/techism.js
+++ static/js/techism.js
@@ -10,3 +10,15 @@ _gaq.push(['_trackPageview']);
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 }) ();
+
+var _paq = _paq || [];
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+(function() {
+    var u="/piwik/";
+    _paq.push(["setTrackerUrl", u+"track"]);
+    _paq.push(["setSiteId", "13"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+})();
