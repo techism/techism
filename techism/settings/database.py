a5f201a60b89d1738647dabf7a0cce6338d987b2
Stefan Seelmann <mail@stefan-seelmann.de>
2015-10-16 23:49:30 +0200

    Fix renamed variables

--- techism/settings/database.py
+++ techism/settings/database.py
@@ -6,8 +6,8 @@ from techism.settings import service as settings_service
 
 try:
     SECRET_KEY = settings_service.get_secret_key()
-    TWITTER_CONSUMER_KEY = settings_service.get_twitter_consumer_key_for_login()
-    TWITTER_CONSUMER_SECRET = settings_service.get_twitter_consumer_secret_for_login()
+    SOCIAL_AUTH_TWITTER_KEY = settings_service.get_twitter_consumer_key_for_login()
+    SOCIAL_AUTH_TWITTER_SECRET = settings_service.get_twitter_consumer_secret_for_login()
 except DatabaseError:
     from django.db import connection
     connection._rollback()
