df9019f712760ae2ae86113494ea10cf8b53a7ef
Stefan Seelmann <mail@stefan-seelmann.de>
2016-04-25 08:13:04 +0200

    Cleanup

--- techism/settings/dev_wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techism.settings.dev")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
--- techism/settings/prod_wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techism.settings.prod")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
